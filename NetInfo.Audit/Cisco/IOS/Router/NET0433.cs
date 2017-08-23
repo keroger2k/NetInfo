443659be5ac8514a02a7dd38855aae87abf424ab
Kyle Rogers <kyle.rogers@gmail.com>
2017-08-22 19:39:57 -0500

    * NetInfo.Audit.csproj: * NetInfo.Parse.csproj: * NetInfo.Devices.csproj: * IR093.cs: * NET0433.cs: * NetInfo.Devices.NMCI.csproj: * NetInfo.Devices.Tests.csproj: * NetInfo.Devices.NMCI.Tests.csproj:
    * NetInfo.Audit.Test.csproj: STIG 0433

---
+++ NetInfo.Audit/Cisco/IOS/Router/NET0433.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using NetInfo.Devices;
+using NetInfo.Devices.IOS;
+using NetInfo.Devices.NMCI.Cisco.IOS;
+
+namespace NetInfo.Audit.Cisco.IOS.Router
+{
+
+    /// <summary>
+    /// Infrastructure Router Security Technical Implementation Guide Cisco :: Release: 23 Benchmark Date: 28 Jul 2017
+    /// 
+    /// Rule Title:   Network devices must use two or more authentication servers for the purpose of granting administrative access.
+    /// STIG ID:    NET0433     
+    /// Rule ID:    SV-16259r4_rule
+    /// Vuln ID:    V-15432       
+    /// Severity:   CAT II  Class:   Unclass
+    /// </summary>
+    public class NET0433 : ICiscoRouterSecurityItem
+    {
+
+        private IIOSDevice _device;
+
+        public NET0433(IIOSDevice device)
+        {
+            this._device = device;
+        }
+
+        public bool Compliant()
+        {
+            return _device.TacacsServer.Hosts.Count() > 1;
+        }
+    }
+}
